51af31b64a0c19701f9e58ad462cddba92c9d229
limaviviiii <vivianny.cruz@escola.pr.gov.br>
2025-09-19 17:42:05 -0300

    final

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const avanca = document.querySelectorAll('.btn-proximo');
+
 avanca.forEach(button => {
     button.addEventListener('click', function(){
         const atual = document.querySelector('.ativo');
         const proximoPasso = 'passo-' + this.getAttribute
+        
         atual.classList.remove('ativo');
         document.getElementById(proximoPasso).classList.add('ativo');
     
